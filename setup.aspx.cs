18031142f1f9a6895887932725c313450e182d70
LeicaSimile <leicasimile@gmail.com>
2017-11-04 20:20:09 -0400

    Make theme chosen persist throughout session

--- setup.aspx.cs
+++ setup.aspx.cs
@@ -7,8 +7,46 @@ using System.Web.UI.WebControls;
 
 public partial class setup : System.Web.UI.Page
 {
+    protected void Page_PreInit(object sender, EventArgs e)
+    {
+        string theme;
+        theme = (string)Session["theme"];
+
+        if (!string.IsNullOrEmpty(theme))
+        {
+            Page.Theme = theme;
+        }
+        else
+        {
+            Page.Theme = "light";
+        }
+    }
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!Page.IsPostBack)
+        {
+            if ("dark" == Page.Theme)
+            {
+                radDark.Checked = true;
+            }
+            else
+            {
+                radLight.Checked = true;
+            }
+        }
+    }
 
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        if (radDark.Checked)
+        {
+            Session["theme"] = "dark";
+        }
+        else
+        {
+            Session["theme"] = "light";
+        }
+        Server.Transfer(Request.FilePath);
     }
 }
