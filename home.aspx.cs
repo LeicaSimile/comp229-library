e2c5de51c5e37ec8f3ff1397b047b522ffbb4373
LeicaSimile <leicasimile@gmail.com>
2017-11-04 20:20:40 -0400

    Set theme on page preinit

--- home.aspx.cs
+++ home.aspx.cs
@@ -7,6 +7,21 @@ using System.Web.UI.WebControls;
 
 public partial class home : System.Web.UI.Page
 {
+    protected void Page_PreInit(object sender, EventArgs e)
+    {
+        string theme;
+        theme = (string)Session["theme"];
+
+        if (!string.IsNullOrEmpty(theme))
+        {
+            Page.Theme = theme;
+        }
+        else
+        {
+            Page.Theme = "light";
+        }
+    }
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
