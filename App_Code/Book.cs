7c2346ffb1fb964e13e29b77aa63091e7a2a8786
LeicaSimile <leicasimile@gmail.com>
2017-11-05 14:57:53 -0500

    Add properties

--- App_Code/Book.cs
+++ App_Code/Book.cs
@@ -8,7 +8,34 @@ using System.Web;
 /// </summary>
 public class Book
 {
-    public Book()
+    private int pages;
+    
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Isbn { get; set; }
+    public string Genre { get; set; }
+    public string Friend { get; set; }
+    public string Comments { get; set; }
+
+    public int Pages
+    {
+        get { return pages; }
+        set
+        {
+            // Minimum number of pages is 1.
+            if (value <= 0)
+            {
+                pages = 1;
+            }
+            else
+            {
+                pages = value;
+            }
+        }
+    }
+
+    public Book(string title, string author, string genre, int pages,
+        string isbn="", string friend="", string comments="")
     {
         //
         // TODO: Add constructor logic here
