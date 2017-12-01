5f62b2e7c153578ad7090eb53ac4fcaf9b585ca7
LeicaSimile <leicasimile@gmail.com>
2017-12-01 18:04:51 -0500

    Use autogenerated script from VS instead

--- App_Data/CreateTableBook.sql
+++ App_Data/CreateTableBook.sql
@@ -1,11 +1,22 @@
-﻿CREATE TABLE [dbo].[Book]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Title] NVARCHAR(100) NOT NULL, 
-    [Author] NVARCHAR(100) NOT NULL, 
-    [Isbn] NVARCHAR(13) NULL, 
-    [Genre] NVARCHAR(50) NULL, 
-    [PageCount] INT NOT NULL, 
-    [FriendLanded] NVARCHAR(100) NULL, 
-    [Comments] NVARCHAR(500) NULL
-)
+﻿USE [LIBRARY.MDF]
+GO
+
+/****** Object: Table [dbo].[Book] Script Date: 12/1/2017 6:02:27 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Book] (
+    [Id]           INT            NOT NULL,
+    [Title]        NVARCHAR (100) NOT NULL,
+    [Author]       NVARCHAR (100) NOT NULL,
+    [Isbn]         NVARCHAR (13)  NULL,
+    [Genre]        NVARCHAR (50)  NULL,
+    [PageCount]    INT            NOT NULL,
+    [FriendLanded] NVARCHAR (100) NULL,
+    [Comments]     NVARCHAR (500) NULL
+);
+
+
