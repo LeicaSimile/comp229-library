40b249ae085183b81e9bf2852a4bb9beb8e77c36
LeicaSimile <leicasimile@gmail.com>
2017-12-01 17:55:39 -0500

    Add Library database

---
+++ App_Data/CreateTableBook.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Book]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NVARCHAR(100) NOT NULL, 
+    [Author] NVARCHAR(100) NOT NULL, 
+    [Isbn] NVARCHAR(13) NULL, 
+    [Genre] NVARCHAR(50) NULL, 
+    [PageCount] INT NOT NULL, 
+    [FriendLanded] NVARCHAR(100) NULL, 
+    [Comments] NVARCHAR(500) NULL
+)
