a3cad5931b536eeb19eab5f5c862ae4462708ede
LeicaSimile <leicasimile@gmail.com>
2017-12-22 12:46:40 -0500

    Display book details in labels

--- book-details.aspx
+++ book-details.aspx
@@ -6,6 +6,14 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="headerContent" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="mainContent" Runat="Server">
+    <h1><asp:Label ID="lblTitle" runat="server" /></h1>
+    Author(s): <asp:Label ID="lblAuthors" runat="server" /><br />
+    ISBN: <asp:Label ID="lblIsbn" runat="server" /><br />
+    Pages: <asp:Label ID="lblPages" runat="server" /><br />
+    Genre: <asp:Label ID="lblGenre" runat="server" /><br />
+    Borrower: <asp:Label ID="lblBorrower" runat="server" /><br />
+    Comments: <asp:Label ID="lblComments" runat="server" /><br />
+
     <asp:Button ID="btnDelete" runat="server" Text="Delete" />
 </asp:Content>
 <%-- Angelica Catalan, 300846458 --%>
--- book-details.aspx.cs
+++ book-details.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -24,6 +26,48 @@ public partial class book_details : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            GetDetails();
+        }
+    }
 
+    private void GetDetails()
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["Library"].ConnectionString;
+        SqlConnection conn = new SqlConnection(connectionString);
+
+        SqlCommand comm = new SqlCommand("select * from Books where Id = @Id", conn);
+        comm.Parameters.AddWithValue("@Id", Request.QueryString["id"]);
+
+        try
+        {
+            conn.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+            reader.Read();
+
+            string title = reader["Title"].ToString();
+            string authors = reader["Authors"].ToString();
+            string isbn = reader["Isbn"].ToString();
+            string pages = reader["Pages"].ToString();
+            string genre = reader["Genre"].ToString();
+            string borrower = reader["Borrower"].ToString();
+            string comments = reader["Comments"].ToString();
+
+            reader.Close();
+
+            this.Title = title;
+            lblTitle.Text = title;
+            lblAuthors.Text = authors;
+            lblIsbn.Text = isbn;
+            lblPages.Text = pages;
+            lblGenre.Text = genre;
+            lblBorrower.Text = borrower;
+            lblComments.Text = comments;
+        }
+        finally
+        {
+            conn.Close();
+        }
     }
 }
